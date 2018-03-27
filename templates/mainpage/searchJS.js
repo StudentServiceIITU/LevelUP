fa4da2fd0410250214d9e29f93dbe5e66a09a92f
osmanaldiyar <osman-aldiyar@mail.ru>
2018-03-27 13:58:09 +0600

    Prototype templates
    Aproximate design

---
+++ templates/mainpage/searchJS.js
@@ -0,0 +1,8 @@
+
+function ChangeColor() {
+  var color = document.getElementById('myColor').value;
+  document.getElementById('ColorCode').style.color = color; 
+  document.getElementById('search-button').style.background = color;
+  document.getElementById("ColorCode").innerHTML = color;
+}
+
