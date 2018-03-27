3abd960ef55ab7c6ada6b51ac5ffe7f211fe5131
osmanaldiyar <osman-aldiyar@mail.ru>
2018-03-27 11:50:34 +0600

    Init project push

---
+++ app/src/main/java/kz/alliance/levelUp/Constants.java
@@ -0,0 +1,12 @@
+package kz.alliance.levelUp;
+
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://osman-aldiyar.000webhostapp.com/Android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+
+
+}
